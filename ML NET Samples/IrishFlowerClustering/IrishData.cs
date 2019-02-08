5418c084ab3c9dd2e259dd66925c45996ab2fb02
Vitaliy Datsyshyn <vitaliy_datsyshyn@epam.com>
2019-02-08 14:32:56 +0200

    - iris flower clustering added

---
+++ ML NET Samples/IrishFlowerClustering/IrishData.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.ML.Data;
+
+namespace IrishFlowerClustering
+{
+    public class IrisData
+    {
+        [Column("0")]
+        public float SepalLength;
+
+        [Column("1")]
+        public float SepalWidth;
+
+        [Column("2")]
+        public float PetalLength;
+
+        [Column("3")]
+        public float PetalWidth;
+    }
+
+    public class ClusterPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public uint PredictedClusterId; // contains the ID of the predicted cluster
+
+        [ColumnName("Score")]
+        public float[] Distances; // contains an array with squared Euclidean distances to the cluster centroids 
+    }
+}
---
+++ ML NET Samples/IrishFlowerClustering/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+
+namespace IrishFlowerClustering
+{
+    public class Program
+    {
+        static readonly string _dataPath = Path.Combine(Environment.CurrentDirectory, "Data", "iris.data");
+        static readonly string _modelPath = Path.Combine(Environment.CurrentDirectory, "Data", "Model.zip");
+
+        static void Main(string[] args)
+        {
+            var mlContext = new MLContext(seed: 0);
+
+            TextLoader textLoader = mlContext.Data.CreateTextReader(new TextLoader.Arguments()
+            {
+                Separator = ",",
+                HasHeader = false,
+                Column = new[]
+                {
+                    new TextLoader.Column("SepalLength", DataKind.R4, 0),
+                    new TextLoader.Column("SepalWidth", DataKind.R4, 1),
+                    new TextLoader.Column("PetalLength", DataKind.R4, 2),
+                    new TextLoader.Column("PetalWidth", DataKind.R4, 3)
+                }
+            });
+
+            IDataView dataView = textLoader.Read(_dataPath);
+
+            var pipeline = mlContext.Transforms.Concatenate("Features", "SepalLength", "SepalWidth", "PetalLength", "PetalWidth")
+                .Append(mlContext.Clustering.Trainers.KMeans("Features", clustersCount: 3));
+
+            var model = pipeline.Fit(dataView);
+
+            using(var fs = new FileStream(_modelPath, FileMode.Create, FileAccess.Write, FileShare.Write))
+            {
+                mlContext.Model.Save(model, fs);
+            }
+
+            var predictor = model.CreatePredictionEngine<IrisData, ClusterPrediction>(mlContext);
+
+            var setosa = new IrisData()
+            {
+                SepalLength = 0.1f,
+                SepalWidth = 0.5f,
+                PetalLength = 0.4f,
+                PetalWidth = 0.2f
+            };
+
+            var prediction = predictor.Predict(setosa);
+
+            Console.WriteLine($"Cluster ID: {prediction.PredictedClusterId}");
+            Console.WriteLine($"Distances: {string.Join(" ", prediction.Distances)}");
+        }
+    }
+}
